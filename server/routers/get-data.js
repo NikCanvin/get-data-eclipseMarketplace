f1462f84c5f617518931ce25c0f771dea93c39cd
Nik Canvin <nik_canvin@uk.ibm.com>
2020-01-20 16:10:54 +0000

    building JSON output

--- server/routers/get-data.js
+++ server/routers/get-data.js
@@ -9,70 +9,56 @@ module.exports = function (app) {
 
     request({
 
-    uri: "https://marketplace.eclipse.org/content/codewind#group-metrics-tab",
+       uri: "https://marketplace.eclipse.org/content/codewind#group-metrics-tab",
 
     }, function(error,response,body){
 
         var bodyArray = body.split(/\r?\n/);
         var foundMetrics="no";
         var gotData;
-        gotData = "Eclipse Marketplace metrics: "+timestamp.utc('YYYY/MM/DD:HH:mm:ss'); 
-        for (i = 0; i < bodyArray.length; i++) {
+        var dataCreatedTimestamp = timestamp.utc('YYYY/MM/DD:HH:mm:ss');
+        var jsonOutput = { campaign: "Codewind", getDataType: "EclispePluginMarketplaceMetrics" };
+        jsonOutput.dataCreatedTimestamp = timestamp.utc('YYYY/MM/DD:HH:mm:ss');
+        jsonOutput.metrics={};
+        monthNumber=0;
+        for (i = 0; i < bodyArray.length; i++) { // find start of metrics data table in the html
             if ( bodyArray[i].includes("<thead><tr><th>Date</th><th>Ranking</th><th>Installs</th><th>Clickthroughs</th> </tr></thead>")) {
                 foundMetrics="yes";
-                //console.log("line"+i+" "+bodyArray[i]);
             }
             if ( foundMetrics=="yes" ) {
                 if ( bodyArray[i].includes("</table>") ) {
                     foundMetrics="no";
                 } else {
-                  gotData += bodyArray[i]+",";
+                    if ( bodyArray[i].includes("thead") || bodyArray[i].includes("tbody")) { //ignore table header and body rows
+                    } else {
+                        // split string on <td>
+                        gotData += bodyArray[i]+",";
+                        var splitBody = bodyArray[i].split("<td>");
+                        var month = splitBody[1].substring(0,splitBody[1].length-5);
+                        var rankingData = splitBody[2].substring(0,splitBody[2].length-5);
+                        var rankingDataPart = rankingData.split("/");
+                        var rankingCurrentValue = rankingDataPart[0];
+                        var rankingOfTotalPlugins = rankingDataPart[1];
+                        var installsData = splitBody[3].substring(0,splitBody[3].length-5);
+                        var installsDataPart = installsData.split(" ");
+                        var installsCurrentValue = installsDataPart[0];
+                        var percentageOfAllInstalls = installsDataPart[1].substring(1,installsDataPart[1].length-2);
+                        var clickThroughs = splitBody[4].substring(0,splitBody[4].length-11);
+                        var monthValue = monthNumber.toString();
+                        var monthlyMetrics = { month: month, rankingCurrentValue: rankingCurrentValue, rankingOfTotalPlugins: rankingOfTotalPlugins, installsCurrentValue: installsCurrentValue, percentageOfAllInstalls: percentageOfAllInstalls, clickThroughs: clickThroughs };
+                        jsonOutput.metrics[monthNumber] = monthlyMetrics;
+                        //jsonOutput.month = month;
+                        var monthNumber = monthNumber+1;
+                    }
                 }
             }
         }
-        res.status(200).send(gotData);
+        res.json(jsonOutput);
+        //res.status(200).send(gotData);
+        }
+    );
 
-    }
-);
-
-
-    //const stringToReturn = "Hey there, this is codewind saying hi and thanks for using me"
-    //res.status(200).send(stringToReturn);
     });
 
   app.use('/get-data', router);
 }
-
-    //router.get('/rules', (req, res) => {
-        // request({
-
-        //     uri: "https://marketplace.eclipse.org/content/codewind#group-metrics-tab",
-
-        //     }, function(error,response,body){
-        
-        //         //console.log(body);
-        //         var bodyArray = body.split(/\r?\n/);
-        //         //console.log (bodyArray.length);
-        //         //var metricsTableStart = bodyArray.indexOf("</div><table class=\"table table-hover table-striped table-bordered sticky-enabled\"></table>");
-        //         //console.log (metricsTableStart);
-        //         var foundMetrics="no";
-        //         console.log("Eclipse Marketplace metrics:")
-        //         console.log(timestamp.utc('YYYY/MM/DD:HH:mm:ss'));
-        //         for (i = 0; i < bodyArray.length; i++) {
-        //             if ( bodyArray[i].includes("<thead><tr><th>Date</th><th>Ranking</th><th>Installs</th><th>Clickthroughs</th> </tr></thead>")) {
-        //                 foundMetrics="yes";
-        //                 //console.log("line"+i+" "+bodyArray[i]);
-        //             }
-        //             if ( foundMetrics=="yes" ) {
-        //                 if ( bodyArray[i].includes("</table>") ) {
-        //                     foundMetrics="no";
-        //                 } else {
-        //                     console.log(bodyArray[i]);
-        //                 }
-        //             }
-        //             //console.log("line"+i+" "+bodyArray[i]);
-        //         }
-
-        //     }
-        // );
-    //});
